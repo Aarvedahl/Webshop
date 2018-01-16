7d1cf187f7cb96adf8ed8c4918fd5687bcbab9c6
Alex <alex.arvedahl@gmail.com>
2018-01-16 12:25:27 +0100

    Draft for security login

--- src/main/resources/public/webshop.js
+++
@@ -1,7 +0,0 @@
-angular.module('products', [])
-  .controller('hello', function($scope, $http) {
-    $http.get('http://rest-service.guides.spring.io/greeting').
-    then(function(response) {
-      $scope.greeting = response.data;
-    });
-  });
