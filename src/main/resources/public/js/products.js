ba5ed470fd02e799fd6cbd1944ddf726591b269f
Alex <alex.arvedahl@gmail.com>
2018-01-18 11:24:28 +0100

    frontend crud is working properly

--- src/main/resources/public/js/products.js
+++ src/main/resources/public/js/products.js
@@ -9,26 +9,39 @@ angular.module('hello', [])
 
         $scope.editArticle = function(article){
             $scope.checked = true;
-            $scope.tempUserData = {
+            $scope.tempArticleData = {
                 articleid:article.articleid,
                 articlename:article.articlename,
                 brand:article.brand,
                 price:article.price,
                 description:article.description
             };
-//            $('.formData').slideDown();
         };
 
-        $scope.addArticle = function () {
-            console.log("WE SHALL ADD A ARTICLE HERE");
+        $scope.addArticle = function (articleToAdd) {
+            console.log("We are supposed to add a article here");
+            console.log(articleToAdd);
+            $scope.checkedFalse();
+            $scope.tempArticleData = {};
         };
 
-        $scope.deleteArticle = function (article) {
+        $scope.deleteArticle = function (articleToDelete) {
             console.log("We are supposed to delete a article here");
-            console.log(article);
+            console.log(articleToDelete);
         };
 
-        $scope.changeChecked = function () {
+        $scope.updateArticle = function (articleToUpdate) {
+            console.log("We are supposed to update a article here");
+            console.log(articleToUpdate);
+            $scope.checkedFalse();
+            $scope.tempArticleData = {};
+        };
+
+        $scope.checkedFalse = function () {
             $scope.checked = false;
-        }
+        };
+
+        $scope.checkedTrue = function () {
+            $scope.checked = true;
+        };
     });
