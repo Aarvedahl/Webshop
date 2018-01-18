ec58961d01e526df9360994768751abc39593038
Alex <alex.arvedahl@gmail.com>
2018-01-18 10:37:58 +0100

    user role entity

---
+++ src/main/java/io/github/aarvedahl/webshop/jpa/User_roles.java
@@ -0,0 +1,30 @@
+package io.github.aarvedahl.webshop.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class User_roles implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String username;
+
+    @Id
+    private String role;
+
+    public User_roles(String username, String role) {
+        this.username = username;
+        this.role = role;
+    }
+
+    public User_roles() { }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+}
--- target/classes/public/js/products.js
+++ target/classes/public/js/products.js
@@ -1,7 +1,34 @@
 angular.module('hello', [])
     .controller('articles', function($scope, $http) {
+        $scope.checked = false;
+
         $http.get('http://localhost:8080/articles').
         then(function(response) {
             $scope.articles = response.data;
         });
+
+        $scope.editArticle = function(article){
+            $scope.checked = true;
+            $scope.tempUserData = {
+                articleid:article.articleid,
+                articlename:article.articlename,
+                brand:article.brand,
+                price:article.price,
+                description:article.description
+            };
+//            $('.formData').slideDown();
+        };
+
+        $scope.addArticle = function () {
+            console.log("WE SHALL ADD A ARTICLE HERE");
+        };
+
+        $scope.deleteArticle = function (article) {
+            console.log("We are supposed to delete a article here");
+            console.log(article);
+        };
+
+        $scope.changeChecked = function () {
+            $scope.checked = false;
+        }
     });
